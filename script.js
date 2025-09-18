14e91ec8d30f67591bc10e0c81b150030cc2460b
agypsynamedTunechi <agypsynamedtunechi@gmail.com>
2025-09-18 11:07:42 +0100

    Refactor gameBoard structure and improve playerTurn function readability

--- script.js
+++ script.js
@@ -2,34 +2,43 @@ function createPlayer(name, marker) {
     return { name, marker };
 }
 
-const gameBoard = function () {
-    const board = ["", "", "",
+const gameBoard = {
+    board: ["", "", "",
         "", "", "",
         "", "", ""
-    ];
+    ]
+}
 
+const gameControl = (function () {
+    const board = gameBoard.board;
     const player1 = createPlayer("Jon", "X");
-        const player2 = createPlayer("Snow", "O");
-        const player = [player1, player2];
-        let currentPlayer = 0;
+    const player2 = createPlayer("Snow", "O");
+    const player = [player1, player2];
+    let currentPlayer = 0;
 
     return function playerTurn(position) {
-         
-        if(board[position] == ""){
+        if (board[position] == "") {
             board[position] = player[currentPlayer].marker;
             console.log(`${player[currentPlayer].name} played in ${position}`)
-            console.log(board)
+            console.table(board)
             currentPlayer = (currentPlayer + 1) % 2;
-        }else{
+        } else {
             console.log(`This position has already been taken`)
         }
 
-        function getCurrentPlayer(){
+        function getCurrentPlayer() {
             return currentPlayer;
         }
-        
+
     };
+})()
+
+const checkWinner = function (){
+    const winningCombination = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+
     
 }
-
-
