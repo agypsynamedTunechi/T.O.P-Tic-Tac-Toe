6ae63d10ae1a6800067b513169790bf69a1b1249
agypsynamedTunechi <agypsynamedtunechi@gmail.com>
2025-09-18 16:10:00 +0100

    Fix initial gameBoard setup and refactor checkWinner function for clarity

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function createPlayer(name, marker) {
 }
 
 const gameBoard = {
-    board: ["", "", "",
+    board: ["X", "X", "X",
         "", "", "",
         "", "", ""
     ]
@@ -16,7 +16,7 @@ const gameControl = (function () {
     const player = [player1, player2];
     let currentPlayer = 0;
 
-    return function playerTurn(position) {
+    function playerTurn(position) {
         if (board[position] == "") {
             board[position] = player[currentPlayer].marker;
             console.log(`${player[currentPlayer].name} played in ${position}`)
@@ -31,14 +31,33 @@ const gameControl = (function () {
         }
 
     };
+    return{playerTurn, player}
 })()
 
 const checkWinner = function (){
+    const board = gameBoard.board;
+    // console.log(board)
     const winningCombination = [
         [0,1,2], [3,4,5], [6,7,8],
         [0,3,6], [1,4,7], [2,5,8],
         [0,4,8], [2,4,6]
     ]
 
+    for(let comb of winningCombination){
+        const [a,b,c] = comb;
+        if(board[a] && board[a] === board[b] && board[a] === board[c]){
+            return console.log(board[a])
+        }
+    } 
+    displayWinner()
     
 }
+
+function displayWinner(){
+    const winner = checkWinner();
+    if(winner === "X"){
+        return console.log(`player1 wins`)
+    }
+}
+
+
