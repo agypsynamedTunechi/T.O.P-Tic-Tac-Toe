65448fa6689f41067699919ba545056d8a1bad8d
agypsynamedTunechi <agypsynamedtunechi@gmail.com>
2025-10-29 15:12:07 +0100

    Enhance styling for player name display and adjust button padding for better usability

--- script.js
+++ script.js
@@ -16,9 +16,52 @@ const Gameboard = (function () {
     return { getBoard, setMove };
 })()
 
-const Game = (function () {
-    const player1 = CreatePlayer();
-    const player2 = CreatePlayer();
+
+const Display = (function () {
+    const cells = document.querySelectorAll(".cell");
+    const player1 = document.querySelector(".player-1");
+    const player2 = document.querySelector(".player-2");
+    const startBtn = document.querySelector(".start-btn");
+    const resetBtn = document.querySelector(".reset-btn");
+    const dialog = document.querySelector("#dialog");
+    const playerNameBoard = document.querySelector(".player-name")
+    const playerOneName = document.querySelector("#player-1-name");
+    const playerTwoName = document.querySelector("#player-2-name");
+    const radios = document.querySelectorAll("input[name='marker']")
+    const selectedRadio = document.querySelector("input[name='marker']:checked")
+    const playBtn = document.querySelector(".submit");
+    const formEl = document.querySelector("form")
+
+    let player1Name = "";
+    let player2Name = "";
+
+
+
+
+    const displayBoard = () => {
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].textContent = Gameboard.getBoard()[i];
+        }
+    }
+
+
+    return {
+        displayBoard, player1,
+        player2, cells,
+        playerNameBoard, playerOneName,
+        playerTwoName, selectedRadio,
+        dialog, resetBtn, startBtn,
+        playBtn, formEl, player1Name,
+        player2Name, radios
+
+    }
+
+})()
+
+const Game = function (user1, user2) {
+    const player1 = user1;
+    const player2 = user2;
+
     let currentPlayer = player1;
 
     const getPlayer1 = () => {
@@ -36,6 +79,7 @@ const Game = (function () {
     const getCurrentPlayer = () => {
         return currentPlayer;
     }
+    console.log(Display.displayBoard())
 
     const checkWinner = (board) => {
         const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
@@ -43,167 +87,178 @@ const Game = (function () {
         for (let comb of winningCombinations) {
             const [a, b, c] = comb;
 
+            // if (Display.cells[a].textContent && Display.cells[a].textContent === Display.cells[b].textContent && Display.cells[a].textContent === Display.cells[c].textContent) {
+            //     console.log(Display.cells[a].textContent)
+            //     return Display.cells[a].textContent;
+
+            // }
+
             if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                console.log(Display.cells[a].textContent);
+                        Display.cells[a].style.backgroundColor = "#008170"
+                        Display.cells[b].style.backgroundColor = "#008170"
+                        Display.cells[c].style.backgroundColor = "#008170"
+                    
+    
                 return board[a];
+
+            } else if (!board.includes("")) {
+                return 'XO';
             }
+
         }
+
     }
 
 
     const displayWinner = (winner) => {
 
         if (winner == player1.marker) {
-            console.log(`${player1.name} wins`)
+            Display.playerNameBoard.textContent = `${player1.name} wins`
         } else if (winner == player2.marker) {
-            console.log(`${player2.name} wins`)
+            Display.playerNameBoard.textContent = `${player2.name} wins`
+            Display.playerNameBoard.style.textAlign = "center"
         } else if (winner = "XO") {
-            console.log("draw")
+            Display.playerNameBoard.textContent = `Draw`
         }
     }
 
 
+    function UpdateScreen() {
 
-    return { getPlayer1, getPlayer2, switchPlayer, getCurrentPlayer, checkWinner, displayWinner }
-})()
+        if (!checkWinner(Gameboard.getBoard())) {
+            if (this.textContent == "") {
+                console.log(getCurrentPlayer())
+                playerTurn(this.dataset.id);
+                Display.displayBoard()
 
-function playerTurn(position) {
-    if (Gameboard.getBoard()[position] === "") {
-        Gameboard.setMove(position, Game.getCurrentPlayer().marker);
-    } else {
-        console.log("This position has been taken!!!")
-    }
+            } else {
+                alert("This Position has been taken")
+            }
 
-    if (Game.checkWinner(Gameboard.getBoard())) {
-        const winner = Game.checkWinner(Gameboard.getBoard());
-        Game.displayWinner(winner);
-    } else if (!Gameboard.getBoard().includes("")) {
-        const winner = "XO";
-        Game.displayWinner(winner);
-    }
-
-    Game.switchPlayer()
-
-}
-
-
-const Display = (function () {
-    const cells = document.querySelectorAll(".cell");
-    const player1 = document.querySelector(".player-1");
-    const player2 = document.querySelector(".player-2");
-    const startBtn = document.querySelector(".start-btn");
-    const resetBtn = document.querySelector(".reset-btn");
-    const dialog = document.querySelector("#dialog");
-    const playerNameBoard = document.querySelector(".player-name")
-    const playerOneName = document.querySelector("#player-1-name");
-    const playerTwoName = document.querySelector("#player-2-name");
-    const xRadio = document.querySelector("#x");
-    const oRadio = document.querySelector("#o");
-    const playBtn = document.querySelector(".submit");
-
-
-    // Hide board when game hasn't started
-
-
-
-    startBtn.addEventListener("click", () => {
-        dialog.showModal();
-    });
-
-    dialog.addEventListener("close", showBoard);
-
-    playBtn.addEventListener("click", (event) => {
-        if (
-            playerOneName.value === "" ||
-            playerTwoName.value === ""
-        ) {
-            return;
-        }
-        event.preventDefault();
-
-        dialog.close();
-        
-    });
-
-
-    cells.forEach(cell => cell.addEventListener("click", UpdateScreen))
-     cells.forEach(cell => cell.addEventListener("click", cellColor))
-     
-    startBtn.addEventListener("click", cellColor)
-    const displayBoard = () => {
-        for (let i = 0; i < cells.length; i++) {
-            cells[i].textContent = Gameboard.getBoard()[i];
+        } else {
+            return
         }
     }
 
+    function cellColor() {
+
+        if (getCurrentPlayer() == getPlayer1()) {
+            Display.player1.style.backgroundColor = "#028391"
+
+            Display.cells.forEach((cell) => {
+                if (cell.textContent == "X") {
+                    cell.style.backgroundColor = "#028391";
+                }
+            });
 
 
 
+        } else {
+            Display.player1.style.backgroundColor = "#232D3F"
+            Display.cells.forEach((cell) => {
+                if (cell.textContent == "X") {
+                    cell.style.backgroundColor = "#232D3F";
+                }
+            });
+        }
 
-    return { displayBoard, player1, player2, cells, playerNameBoard}
+        if (getCurrentPlayer() == getPlayer2()) {
+            Display.player2.style.backgroundColor = "#008170"
 
-})()
+            Display.cells.forEach((cell) => {
+                if (cell.textContent == "O") {
+                    cell.style.backgroundColor = "#008170";
+                }
+            });
 
 
 
-function UpdateScreen() {
-    if (this.textContent == "") {
-        console.log(Game.getCurrentPlayer())
-        playerTurn(this.dataset.id);
-        Display.displayBoard()
-    } else {
-        alert("This Position has been taken")
+        } else {
+            Display.player2.style.backgroundColor = "#232D3F"
+            Display.cells.forEach((cell) => {
+                if (cell.textContent == "O") {
+                    cell.style.backgroundColor = "#232D3F";
+                }
+            });
+        }
+
     }
 
-
-}
-
-function cellColor() {
-   
-    if (Game.getCurrentPlayer() == Game.getPlayer1()) {
-        Display.player1.style.backgroundColor = "#028391"
- 
-        Display.cells.forEach((cell) =>{
-            if(cell.textContent == "X"){
-                cell.style.backgroundColor = "#028391";
-            }
-        });
-        
-
-
-    }else{
-        Display.player1.style.backgroundColor = "#232D3F"
-         Display.cells.forEach((cell) =>{
-            if(cell.textContent == "X"){
-                cell.style.backgroundColor = "#232D3F";
-            }
-        });
-    }
-
-    if (Game.getCurrentPlayer() == Game.getPlayer2()) {
-        Display.player2.style.backgroundColor = "#008170"
- 
-        Display.cells.forEach((cell) =>{
-            if(cell.textContent == "O"){
-                cell.style.backgroundColor = "#008170";
-            }
-        });
-        
-
-
-    }else{
-        Display.player2.style.backgroundColor = "#232D3F"
-         Display.cells.forEach((cell) =>{
-            if(cell.textContent == "O"){
-                cell.style.backgroundColor = "#232D3F";
-            }
-        });
-    }
-
-}
-
-    function showBoard(){
+    function showBoard() {
         console.log(Display.cells)
-      Display.cells.forEach(cell => cell.classList.toggle("visible"));
-      Display.playerNameBoard.classList.toggle("visible");
+        Display.cells.forEach(cell => cell.classList.toggle("visible"));
+        Display.playerNameBoard.classList.toggle("visible");
     }
+
+    function playerTurn(position) {
+
+        if (Gameboard.getBoard()[position] === "") {
+            Gameboard.setMove(position, getCurrentPlayer().marker);
+        } else {
+            console.log("This position has been taken!!!")
+        }
+
+        if (checkWinner(Gameboard.getBoard())) {
+            const winner = checkWinner(Gameboard.getBoard());
+            displayWinner(winner);
+            return
+        }
+         switchPlayer()
+    }
+
+    Display.dialog.addEventListener("close", showBoard);
+    Display.cells.forEach(cell => cell.addEventListener("click", UpdateScreen))
+    Display.cells.forEach(cell => cell.addEventListener("click", cellColor))
+
+
+    return { cellColor }
+}
+
+
+
+Display.startBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+let player1marker = ""
+
+Display.radios.forEach((radio) => {
+    radio.addEventListener("change", function () {
+        player1marker = document.querySelector("input[name='marker']:checked").value
+    })
+})
+
+
+Display.playBtn.addEventListener("click", (event) => {
+    if (
+        Display.playerOneName.value === "" ||
+        Display.playerTwoName.value === ""
+    ) {
+        return;
+    }
+    event.preventDefault();
+
+
+    const player1name = Display.playerOneName.value;
+    const player2name = Display.playerTwoName.value;
+    console.log(player1name)
+    const player1 = CreatePlayer(player1name, player1marker)
+    const player2 = CreatePlayer(player2name, player1marker == "X" ? "O" : "X");
+
+    const game = Game(player1, player2)
+    game.cellColor()
+
+    Display.dialog.close();
+
+});
+
+Display.formEl.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+})
+
+
+
+
 
